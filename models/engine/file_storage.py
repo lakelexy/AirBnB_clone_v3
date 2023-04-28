3223eb22c2504ce2cdae8e95e3b6621e2a6bc5c3
lakelexy <alao8380@yahoo.com>
2023-04-28 12:00:49 +0000

    add get and count

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -38,7 +38,7 @@ class FileStorage:
         """get an object based on its class and id"""
 
         my_dict = self.all(cls)
-        for obj in my_dict.value():
+        for obj in my_dict.values():
             if obj.id == id:
                 return obj
         return None
