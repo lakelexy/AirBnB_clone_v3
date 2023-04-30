628f7b662ae41b1182cd69cfb7fb5d21692c802b
lakelexy <alao8380@yahoo.com>
2023-04-30 15:09:04 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,21 +5,28 @@
 from api.v1.views import app_views
 from flask import Flask
 from flask import jsonify
+from os import getenv
 from models import storage
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def clean_up(exception=None):
     """eliminates current Session"""
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found_error(error):
     """handles 404 error"""
-    return jsonify({"error": "Not found"})
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    host = getenv('HBNB_API_HSOT', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+
+    app.run(host, int(port), threaded=True)
